4b9621813a103b3619f0695474285c5f4d74eb07
Alex Santini <47783526+AlexSantini10@users.noreply.github.com>
2024-06-18 14:47:13 +0200

    fix(client): :sparkles: Error page messa a posto

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,17 +1,25 @@
-import React from 'react'
-
-import {Link} from 'react-router-dom';
+import React, { act, useEffect } from 'react'
+import { LogoutButton } from '../widgets'
+import { useAppContext } from '../context/appContext'
+import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import img from '../assets/images/not-found.svg'
 
+import {
+  DropDown, Logo, LogoAndTitle, PrimaryButton, TextInput, OutlinedButton, Header, PulsanteConfermaAzzurro, PulsanteConfermaRosso, PulsanteAnnulla, PulsanteImmagine,
+  NavBar, MyIcon, TopArea, PaperList, TornaAllaHome, ButtonArea, SaldoHome
+} from '../components'
 const Error = () => {
   return (
-      <div>
-        <img src={img} alt='not found' />
-        <h3>Oh! Page not found</h3>
+    <div style={{ width: "60vh", height: "100vh", margin: 'auto', display: 'flex', flexDirection: 'column' }}>
+        <img src={img} alt='not found' style={{marginTop:"30%"}} />
+        <h3>Oh no! Page not found</h3>
         <p>We can't find the page you are looking for</p>
         <Link to='/'>Back Home</Link>
-      </div>
+    </div>
   )
 }
-
+/*
+  
+*/
 export default Error
